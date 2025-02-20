109a6e8f702abbc228e87a93cebb4bc03ee65699
Ulfred Chan <101036368+undarr@users.noreply.github.com>
2025-02-20 16:07:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 from bs4 import BeautifulSoup
 
@@ -9,7 +12,7 @@ from bs4 import BeautifulSoup
 st.set_page_config(layout="wide")
 
 # Keep text only
-def get_website_content(url):
+def get_website_content():
     driver = None
     try:
         # Using on Local
@@ -19,21 +22,28 @@ def get_website_content(url):
         options.add_argument('--window-size=1920,1200')
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                                   options=options)
-        st.write(f"DEBUG:DRIVER:{driver}")
-        driver.get(url)
-        time.sleep(1)
-        html_doc = driver.title
+        driver.get("https://www.minutecryptic.com")
+        button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-sentry-component="SerifVariant"]'))
+        )
+        # Click the button
+        button.click()
+        # Retrieve local storage data
+        puzzle_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-sentry-component="PuzzlePiece"]'))
+        )
+        # Click the second button
+        puzzle_button.click()
+        puzzle_button.send_keys('a')
+        local_storage = driver.execute_script("return window.localStorage;")
         driver.quit()
-        return html_doc
+        return local_storage
     except Exception as e:
         st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
     finally:
         if driver is not None: driver.quit()
     return None
 
-
-
-
 # ---------------- Page & UI/UX Components ------------------------
 if __name__ == "__main__":
-    st.write(get_website_content("https://www.minutecryptic.com"))
+    st.write(get_website_content())
