9308783c610ff668e5c3b38b828da99504c4c33d
Ulfred Chan <101036368+undarr@users.noreply.github.com>
2026-02-13 01:58:38 +0800

    Add import for ChromeType from webdriver_manager

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.core.os_manager import ChromeType
 import time
 import json
 import re
